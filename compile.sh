c3193893a17fe90c13e776d435ecf597ac991f71
“wyj-home-ubuntu-22.04” <2211819818@qq.com>
2024-10-21 22:29:25 +0800

    add stm32 jtag-gpio config

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 if [ "$1" = "flash" ]; then
     st-flash --connect-under-reset write build/zephyr/zephyr.bin 0x8000000
 elif [ "$1" = "all" ]; then
+    west build -p always -b stm32_min_dev_blue  ./FlexiAssistGlove
     west build -p always -b stm32f030c8t6  ./FlexiAssistGlove
 else
     west build -p auto -b stm32f030c8t6  ./FlexiAssistGlove
--- src/main.c
+++ src/main.c
@@ -5,14 +5,16 @@
 #include "air_sensor.h"
 #include "imu.h"
 #include "pwm.h"
+#include "stm32_ll_gpio.h"
 
 int main(void)
 {
 	printf("main start !\n");
+	__HAL_AFIO_REMAP_SWJ_NOJTAG();
 
 	pwm_device_init();
 	// imu_init();
-	// air_sensor_init();
+	//air_sensor_init();
 
 	return 0;
 }
